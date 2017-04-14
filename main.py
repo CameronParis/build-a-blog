4ab447a7a34fadf119f9855a2ed314624e25b034
Cameron Paris <Cameron.Paris@gmail.com>
2017-04-13 20:09:59 -0500

    Created Handler class

--- main.py
+++ main.py
@@ -21,21 +21,33 @@ import os
 import re
 from google.appengine.ext import db
 
+# set up jinja
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
 
-class MainHandler(webapp2.RequestHandler):
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+
+class MainPage(Handler):
     def get(self):
         self.response.write('Hello world!')
 
-class Blog(db.Model):
+#class Blog(db.Model):
 
-class NewPost(db.Model):
+#class NewPost(db.Model):
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/blog', Blog),
-    ('/newpost', NewPost)
+    ('/', MainPage),
+    #('/blog', Blog),
+    #('/newpost', NewPost)
 ], debug=True)
